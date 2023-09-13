457f799c272ab6799aa8911cd78bf48ba9e6729a
ChezyName <mineboy2468@gmail.com>
2023-09-13 13:36:59 -0500

    Repalced Colors to YouTube Colors & Top Nav Added

--- src/components/NavPannel/RightNavPannel.jsx
+++ src/components/NavPannel/RightNavPannel.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import UserImage from "./EmptyUser.png"
+import YTPlanner from "./YouTubePlanrIcon.png"
 
 const RightNavPannel = () => {
+
+  function goToChannel(){
+    console.log("Opening Channel")
+  }
+
   return (
-    <div id="rNav">
-        <div id="userInfo" style={{height: "8%", borderBottom: "6px solid #141414", display: 'flex'}}>
-            <img style={{height: "100%", width: "auto", borderRight: '6px solid #141414'}} src={UserImage}></img>
-            <div style={{width: "100%", height:"100%", display: 'grid', justifyContent: 'center', alignContent: 'center'}}>
-              <span style={{fontSize: '3vw', whiteSpace: "nowrap", overflow: 'hidden', textOverflow: 'ellipsis'}}>Username</span>
-            </div>
-        </div>
+    <div id="tNav">
+      <img src={YTPlanner} style={{height: '80%', width: 'auto', marginRight: 'auto', userSelect: 'none'}}></img>
+      <img src={UserImage} onClick={goToChannel} style={{height: '100%', width: 'auto', userSelect: 'none'}}></img>
     </div>
   )
 }
